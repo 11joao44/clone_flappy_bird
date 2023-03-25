7811a5506ab00c97c9317f5c19e73101df59ca29
João Pedro <11joao44@gmail.com>
2023-03-25 03:57:46 -0400

    adicionando tela de inicio e click play

--- main.js
+++ main.js
@@ -15,51 +15,48 @@ const planoDeFundo = {
       contexto.fillStyle = '#70c5ce';
       contexto.fillRect(0,0, canvas.width, canvas.height)
   
-      contexto.drawImage(
-        sprites,
-        planoDeFundo.spriteX, planoDeFundo.spriteY,
-        planoDeFundo.largura, planoDeFundo.altura,
-        planoDeFundo.x, planoDeFundo.y,
-        planoDeFundo.largura, planoDeFundo.altura,
-      );
-  
-      contexto.drawImage(
-        sprites,
-        planoDeFundo.spriteX, planoDeFundo.spriteY,
-        planoDeFundo.largura, planoDeFundo.altura,
-        (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-        planoDeFundo.largura, planoDeFundo.altura,
-      );
+      for (let i = 0; i < canvas.width; i += planoDeFundo.largura) {
+
+          contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, 
+            planoDeFundo.spriteY,
+            planoDeFundo.largura, 
+            planoDeFundo.altura,
+            planoDeFundo.x + i, 
+            planoDeFundo.y,
+            planoDeFundo.largura, 
+            planoDeFundo.altura,
+          );
+      }
     },
   };
 
-const chao = {
+  const chao = {
     spritesX: 0,
     spritesY: 610,
     largura: 224,
     altura: 112,
     x: 0,
     y: canvas.height - 112,
-
+  
     desenha() {
+      for (let i = 0; i < canvas.width; i += chao.largura) {
         contexto.drawImage(
-            sprites,
-            chao.spritesX, chao.spritesY,
-            chao.largura, chao.altura,
-            chao.x, chao.y,
-            chao.largura, chao.altura,
+          sprites,
+          chao.spritesX,
+          chao.spritesY,
+          chao.largura,
+          chao.altura,
+          chao.x + i,
+          chao.y,
+          chao.largura,
+          chao.altura
         );
-    
-        contexto.drawImage(
-            sprites,
-            chao.spritesX, chao.spritesY,
-            chao.largura, chao.altura,
-            (chao.x + chao.largura), chao.y,
-            chao.largura, chao.altura,
-        );
+      }
     }
-}
-
+  };
+  
 
 const bird = {
     spritesX: 0,
@@ -87,14 +84,81 @@ const bird = {
     }
 }
 
-function loop() {
-    planoDeFundo.desenha();
-    chao.desenha();
-    bird.desenha();
-    bird.atualiza();
+const mensagemGetReady = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 250,
+    dWidth: 1000,
+    dHeigth: 1000,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.sX,
+            mensagemGetReady.sY,
+            mensagemGetReady.w,
+            mensagemGetReady.h,
+            mensagemGetReady.x,
+            mensagemGetReady.y,
+            mensagemGetReady.w,
+            mensagemGetReady.h,
+            mensagemGetReady.dWidth,
+            mensagemGetReady.dHeigth,
+        )
+    }
+}
 
+let telaAtiva = {};
+function mudaParaTela(novaTele) {
+    telaAtiva = novaTele;
+}
+
+const Telas = {
+    incio: {
+        desenha() {
+            planoDeFundo.desenha();
+            mensagemGetReady.desenha();
+            chao.desenha();
+            bird.desenha();
+        },
+        
+        click() {
+            mudaParaTela(Telas.JOGO)
+        },
+
+        atualiza() {
+
+        }
+    }
+};
+
+Telas.JOGO = {
+    desenha() {
+        planoDeFundo.desenha();
+        chao.desenha();
+        bird.desenha();
+    },
+
+    atualiza() {
+        bird.atualiza();
+    }
+}
+
+function loop() {
+
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
     
     requestAnimationFrame(loop)
 }
 
+window.addEventListener('click', function() {
+   if (telaAtiva.click) {
+    telaAtiva.click()
+   }
+})
+
+mudaParaTela(Telas.incio)
 loop();
